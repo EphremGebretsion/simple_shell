cba705133cbb381bd493408ed18de6f6bfab7f45
= <=>
2023-11-16 02:01:37 +0300

    get it man

--- shell.c
+++ shell.c
@@ -6,44 +6,37 @@
 #include "main.h"
 
 /**
- * shell_loop - used to loop our shell
- * @envi: the environment to use
- * @prog: the program name being excuted
+ * shell - excutes our shell program prompt, getcommand, excute
+ * @envi: acepts environment from main and use it
+ * @prog: the name of the program used to excute
  */
-void shell_loop(char **envi, char *prog)
+
+void shell(char **envi, char *prog)
 {
 	size_t line_size = 20;
 	char *line = malloc(sizeof(char) * line_size);
 	char *line_array[] = {NULL, NULL};
+	int last_char;
 	ssize_t len;
-	int wait_status;
-	pid_t child = 1;
 
-	while (child)
+	printf("#cisfun$ ");
+	len = getline(&line, &line_size, stdin);
+	if (len != -1)
 	{
-		printf("#cisfun$ ");
-		len = getline(&line, &line_size, stdin);
-		if (len != -1)
-		{
-			child = fork();
-			wait(&wait_status);
-
-			if (!child)
-			{
-				line_array[0] = strtok(line, "\n");
-				if (execve(line_array[0], line_array, envi) == -1)
-					perror((const char *)prog);
-			}
-		}
+		last_char = strlen(line);
+		if (line[last_char - 1] != 10)
+			printf("\n");
 		else
 		{
-			printf("\n");
-			child = 0;
+			line_array[0] = strtok(line, "\n");
+			if (execve(line_array[0], line_array, envi) == -1)
+				printf("%s: No such file or directory\n", prog);
 		}
 	}
+	else
+		printf("\n");
 	free(line);
 }
-
 /**
  * main - excutes our simple shell
  * @ac: number of arguments passed
@@ -54,8 +47,22 @@ void shell_loop(char **envi, char *prog)
 
 int main(int ac, char **av, char **env)
 {
+	pid_t child = 1;
+	int wait_status;
+
 	(void)ac;
-	shell_loop(env, av[0]);
+	if (isatty(0))
+	{
+		while (child)
+		{
+			child = fork();
+			wait(&wait_status);
+			if (!child)
+				shell(env, av[0]);
+		}
+	}
+	else
+		shell(env, av[0]);
+
 	return (0);
 }
-
