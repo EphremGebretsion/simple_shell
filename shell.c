ada0cfab102c91dd8a0a2ef145d3a31f7a4920f2
= <=>
2023-11-16 01:43:08 +0300

    shell huff

--- shell.c
+++ shell.c
@@ -6,30 +6,44 @@
 #include "main.h"
 
 /**
- * shell - excutes our shell program prompt, getcommand, excute
- * @envi: acepts environment from main and use it
- * @prog: the name of the program used to excute
+ * shell_loop - used to loop our shell
+ * @envi: the environment to use
+ * @prog: the program name being excuted
  */
-
-void shell(char **envi, char *prog)
+void shell_loop(char **envi, char *prog)
 {
 	size_t line_size = 20;
 	char *line = malloc(sizeof(char) * line_size);
 	char *line_array[] = {NULL, NULL};
 	ssize_t len;
+	int wait_status;
+	pid_t child = 1;
 
-	printf("#cisfun$ ");
-	len = getline(&line, &line_size, stdin);
-	if (len != -1)
+	while (child)
 	{
-		line_array[0] = strtok(line, "\n");
-		if (execve(line_array[0], line_array, envi) == -1)
-			perror((const char *)prog);
+		printf("#cisfun$ ");
+		len = getline(&line, &line_size, stdin);
+		if (len != -1)
+		{
+			child = fork();
+			wait(&wait_status);
+
+			if (!child)
+			{
+				line_array[0] = strtok(line, "\n");
+				if (execve(line_array[0], line_array, envi) == -1)
+					perror((const char *)prog);
+			}
+		}
+		else
+		{
+			printf("\n");
+			child = 0;
+		}
 	}
-	else
-		printf("\n");
 	free(line);
 }
+
 /**
  * main - excutes our simple shell
  * @ac: number of arguments passed
@@ -40,22 +54,8 @@ void shell(char **envi, char *prog)
 
 int main(int ac, char **av, char **env)
 {
-	pid_t child = 1;
-	int wait_status;
-
 	(void)ac;
-	if (isatty(0))
-	{
-		while (child)
-		{
-			child = fork();
-			wait(&wait_status);
-			if (!child)
-				shell(env, av[0]);
-		}
-	}
-	else
-		shell(env, av[0]);
-
+	shell_loop(env, av[0]);
 	return (0);
 }
+
