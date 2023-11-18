085c4eaed1d1c8139215df877c3fcc1ec55fd531
EPHREM GEBRETISON <ephhrem@gmail.com>
2023-11-18 03:23:17 +0300

    ll

--- main.h
+++ main.h
@@ -1,6 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-int shell(char **envi, int prompt, char *prog);
-int shell_exec(char **envi, char *prog, int prompt);
-int exe(char **comand, char **envi, char *prog);
+
 #endif
